243daa9556a54eb2be51fca546498d67457855b7
ubernaut <collin.schroeder@gmail.com>
2025-10-30 16:49:37 -0800

    fixed relative path for gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-//  plugins: [basicSsl()],
+  base: './',
   server: {
     host: '0.0.0.0', // Listen on all network interfaces
     port: 5173,
